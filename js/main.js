57c5d221c2db9fa88515da62764dbf41833c7e7a
lalyos <lalyos@yahoo.com>
2013-11-13 17:02:38 +0100

    more sample boulders added

--- js/main.js
+++ js/main.js
@@ -2,10 +2,36 @@ function BoulderCtrl($scope) {
     
     $scope.loggedIn = false;
   $scope.boulders = [
-    {id:1, name:'kalacs', diff:'red',  climbed:false, tries:0},
-    {id:2, name:'hokifli',diff:'green', climbed:false, tries:0},
-    {id:3, name:'minyon',diff:'green', climbed:false, tries:0},
-    {id:4, name:'isler',diff:'yellow', climbed:false, tries:0}
+    {id:1, name:'midnight', diff:'red',  climbed:false, tries:0},
+    {id:2, name:'lighting', diff:'red',  climbed:false, tries:0},
+    {id:3, name:'hókifli',diff:'green', climbed:false, tries:0},
+    {id:4, name:'baklava',diff:'green', climbed:false, tries:0},
+    {id:5, name:'zserbó',diff:'green', climbed:false, tries:0},
+    {id:6, name:'piskóta',diff:'green', climbed:false, tries:0},
+    {id:7, name:'beigli',diff:'green', climbed:false, tries:0},
+    {id:8, name:'krémes',diff:'green', climbed:false, tries:0},
+    {id:9, name:'Günter',diff:'green', climbed:false, tries:0},
+    {id:10, name:'Linzer',diff:'green', climbed:false, tries:0},
+    {id:11, name:'Ishler',diff:'green', climbed:false, tries:0},
+    {id:12, name:'Csörlő',diff:'green', climbed:false, tries:0},
+    {id:13, name:'Málna',diff:'yellow', climbed:false, tries:0},
+    {id:14, name:'Dió',diff:'yellow', climbed:false, tries:0},
+    {id:15, name:'Alma',diff:'yellow', climbed:false, tries:0},
+    {id:16, name:'Pisztácia',diff:'yellow', climbed:false, tries:0},
+    {id:17, name:'Mák',diff:'yellow', climbed:false, tries:0},
+    {id:18, name:'Gesztenye',diff:'yellow', climbed:false, tries:0},
+    {id:19, name:'Vanilia',diff:'yellow', climbed:false, tries:0},
+    {id:20, name:'Csokoládé',diff:'yellow', climbed:false, tries:0},
+    {id:21, name:'Laplace',diff:'yellow', climbed:false, tries:0},
+    {id:22, name:'pucs',diff:'yellow', climbed:false, tries:0},
+    {id:23, name:'csutka',diff:'yellow', climbed:false, tries:0},
+    {id:24, name:'dó',diff:'blue', climbed:false, tries:0},
+    {id:25, name:'ré',diff:'blue', climbed:false, tries:0},
+    {id:26, name:'mi',diff:'blue', climbed:false, tries:0},
+    {id:27, name:'fá',diff:'blue', climbed:false, tries:0},
+    {id:28, name:'szó',diff:'blue', climbed:false, tries:0},
+    {id:29, name:'ti',diff:'blue', climbed:false, tries:0},
+    {id:30, name:'lá',diff:'blue', climbed:false, tries:0}
   ];
   
   $scope.climbed = function() {
